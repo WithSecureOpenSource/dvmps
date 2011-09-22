1bf436e66ea9df232c50be6b95543ca9a17e3ded
Heikki Nousiainen <Heikki.Nousiainen@F-Secure.com>
2011-09-22 17:48:55 +0800

    SQL format mac_ip_pair and base_images definitions for GTN deployment

---
+++ misc/gtn-10.133.13.0-base_images.sql
@@ -0,0 +1,6 @@
+insert into base_images (base_image_name, configuration_template, base_image_file) values ('noushe-win7-x64', 'template-noushe-win7-x64.xml', 'noushe-win7-x64.qcow2');
+insert into base_images (base_image_name, configuration_template, base_image_file) values ('noushe-win7-x32', 'template-noushe-win7-x32.xml', 'noushe-win7-x32.qcow2');
+insert into base_images (base_image_name, configuration_template, base_image_file) values ('noushe-vista-x64', 'template-noushe-vista-x64.xml', 'noushe-vista-x64.qcow2');
+insert into base_images (base_image_name, configuration_template, base_image_file) values ('noushe-vista-x32', 'template-noushe-vista-x32.xml', 'noushe-vista-x32.qcow2');
+insert into base_images (base_image_name, configuration_template, base_image_file) values ('noushe-winxp', 'template-noushe-winxp.xml', 'noushe-winxp.qcow2');
+insert into base_images (base_image_name, configuration_template, base_image_file) values ('noushe-winxp-ger', 'template-noushe-winxp-ger.xml', 'noushe-winxp-ger.qcow2');
