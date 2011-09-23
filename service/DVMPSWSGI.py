6c3f35ba22d6c8db067d6b79733801d203e637ab
Heikki Nousiainen <Heikki.Nousiainen@F-Secure.com>
2011-09-23 11:28:55 +0800

    Split WSGI functionality separate from WSGIRef implementation with anticipation to more complete WSGI framework support.

---
+++ service/DVMPSWSGI.py
@@ -0,0 +1,73 @@
+import DVMPSService
+import json
+
+class DVMPSWSGI:
+    def __init__(self, database=None):
+        self.dvmps = DVMPSService.DVMPSService(database=database)
+
+    def dvmps_app(self, environ, start_response):
+        command = environ['PATH_INFO']
+
+        status = '200 OK'
+        headers = [('Content-type', 'text/plain')]
+
+        start_response(status, headers)
+
+        res = None
+        request_params = None
+
+        if self.dvmps is not None and environ['REQUEST_METHOD'] == 'POST' and environ.has_key('CONTENT_LENGTH'):
+            data_size = int(environ['CONTENT_LENGTH'])
+            data = environ['wsgi.input'].read(data_size)
+            request_params = json.loads(data)
+
+            if command == '/allocate':
+                base_image = None
+                expires = None
+                comment = None
+                if request_params.has_key('base_image'):
+                    base_image = request_params['base_image']
+                if request_params.has_key('expires'):
+                    expires = request_params['expires']
+                if request_params.has_key('comment'):
+                    comment = request_params['comment']
+                if base_image is not None and expires is not None:
+                    res = self.dvmps.allocate_image(base_image, expires, comment)
+            elif command == '/deallocate':
+                image_id = None
+                if request_params.has_key('image_id'):
+                    image_id = request_params['image_id']
+                if image_id is not None:
+                    res = self.dvmps.deallocate_image(image_id)
+            elif command == '/revert':
+                image_id = None
+                if request_params.has_key('image_id'):
+                    image_id = request_params['image_id']
+                if image_id is not None:
+                    res = self.dvmps.revert_image(image_id)
+            elif command == '/poweroff':
+                image_id = None
+                if request_params.has_key('image_id'):
+                    image_id = request_params['image_id']
+                if image_id is not None:
+                    res = self.dvmps.poweroff_image(image_id)
+            elif command == '/poweron':
+                image_id = None
+                if request_params.has_key('image_id'):
+                    image_id = request_params['image_id']
+                if image_id is not None:
+                    res = self.dvmps.poweron_image(image_id)
+            elif command == '/status':
+                image_id = None
+                if request_params.has_key('image_id'):
+                    image_id = request_params['image_id']
+                if image_id is not None:
+                    res = self.dvmps.image_status(image_id)
+            elif command == '/systemstatus':
+                res = self.dvmps.status()
+            elif command == '/running_images':
+                res = self.dvmps.running_images()
+            elif command == '/base_images':
+                res = self.dvmps.base_images()
+
+        return [json.dumps(res)]
--- service/DVMPSWSGIRef.py
+++ service/DVMPSWSGIRef.py
@@ -1,76 +1,15 @@
 from wsgiref.simple_server import make_server
-import DVMPSService
-import json
+import DVMPSWSGI
 
-vma = None
+dvmps_wsgi = None
 
-def vm_allocator_app(environ, start_response):
-    command = environ['PATH_INFO']
-
-    status = '200 OK'
-    headers = [('Content-type', 'text/plain')]
-
-    start_response(status, headers)
-
-    res = None
-    request_params = None
-
-    if vma is not None and environ['REQUEST_METHOD'] == 'POST' and environ.has_key('CONTENT_LENGTH'):
-        data_size = int(environ['CONTENT_LENGTH'])
-        data = environ['wsgi.input'].read(data_size)
-        request_params = json.loads(data)
-
-        if command == '/allocate':
-            base_image = None
-            expires = None
-            comment = None
-            if request_params.has_key('base_image'):
-                base_image = request_params['base_image']
-            if request_params.has_key('expires'):
-                expires = request_params['expires']
-            if request_params.has_key('comment'):
-                comment = request_params['comment']
-            if base_image is not None and expires is not None:
-                res = vma.allocate_image(base_image, expires, comment)
-        elif command == '/deallocate':
-            image_id = None
-            if request_params.has_key('image_id'):
-                image_id = request_params['image_id']
-            if image_id is not None:
-                res = vma.deallocate_image(image_id)
-        elif command == '/revert':
-            image_id = None
-            if request_params.has_key('image_id'):
-                image_id = request_params['image_id']
-            if image_id is not None:
-                res = vma.revert_image(image_id)
-        elif command == '/poweroff':
-            image_id = None
-            if request_params.has_key('image_id'):
-                image_id = request_params['image_id']
-            if image_id is not None:
-                res = vma.poweroff_image(image_id)
-        elif command == '/poweron':
-            image_id = None
-            if request_params.has_key('image_id'):
-                image_id = request_params['image_id']
-            if image_id is not None:
-                res = vma.poweron_image(image_id)
-        elif command == '/status':
-            image_id = None
-            if request_params.has_key('image_id'):
-                image_id = request_params['image_id']
-            if image_id is not None:
-                res = vma.image_status(image_id)
-        elif command == '/systemstatus':
-            res = vma.status()
-
-    return [json.dumps(res)]
+def wsgi_app(environ, start_response):
+    return dvmps_wsgi.dvmps_app(environ, start_response)
 
 if __name__ == '__main__':
-    vma = DVMPSService.DVMPSService(database='dvmps')
+    dvmps_wsgi = DVMPSWSGI.DVMPSWSGI(database='dvmps')
 
-    httpd = make_server('', 80, vm_allocator_app)
+    httpd = make_server('', 80, wsgi_app)
     print "Serving on port 80..."
 
     # Serve until process is killed
