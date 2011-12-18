477f17b74e5e194c66d15c291bb82fb37036e229
Heikki Nousiainen <Heikki.Nousiainen@F-Secure.com>
2011-12-18 17:23:26 +0800

    Placement agent, v2: calculate scores for willingness to run images already at local node. This saves bunch of processing while devising the placement strategy on all participating nodes. Values are arbitrary: positive indicating willingness and negative indicating capability but no priority.

--- service/DVMPSPlacementAgent.py
+++ service/DVMPSPlacementAgent.py
@@ -10,7 +10,7 @@ import sys
 
 logger = logging.getLogger('dvmps-pa')
 
-received_node_data = {}
+received_node_scores = {}
 
 __appname__ = "dvmps-placement-agent"
 __usage__ = "%prog [-l <logfile>]"
@@ -25,29 +25,26 @@ class UDPHandler(SocketServer.BaseRequestHandler):
         except:
             logger.warn("non-json data from %s" % self.client_address[0])
             return
-        if type(node_data).__name__ != 'dict' or not node_data.has_key('type') or node_data['type'] != 'dvmps_node_update_v1' or not node_data.has_key('images'):
+        if type(node_data).__name__ != 'dict' or not node_data.has_key('type') or node_data['type'] != 'dvmps_node_update_v2' or not node_data.has_key('image_scores'):
             logger.warn("Data from %s wasn't of the correct type, or was missing mandatory fields" % self.client_address[0])
             return
-        node_images = {}
-        for candidate in node_data['images']:
+        node_scores = {}
+        for candidate in node_data['image_scores']:
             if type(candidate).__name__ == 'dict' and \
-               candidate.has_key('running_instances') and type(candidate['running_instances']).__name__ == 'int' and \
+               candidate.has_key('score') and type(candidate['score']).__name__ == 'int' and \
                candidate.has_key('base_image_name'):
                 if type(candidate['base_image_name']).__name__ == 'str' or type(candidate['base_image_name']).__name__ == 'unicode':
-                    node_images[str(candidate['base_image_name'])] = candidate['running_instances']
+                    node_scores[str(candidate['base_image_name'])] = candidate['score']
                 else:
                     logger.warn("Invalid type for base_image_name '%s' from %s" % (type(candidate['base_image_name']).__name__, self.client_address[0]))
             else:
                 logger.warn("Node image candidate from %s: one or more missing fields or invalid types" % self.client_address[0])
         node_name = self.client_address[0]
         now = int(time.time())
-        received_node_data[node_name] = {'images':node_images,'timestamp':now}
+        received_node_scores[node_name] = {'image_scores':node_scores,'timestamp':now}
 
-        node_image_keys = node_images.keys()
-        count = 0
-        for node_image_key in node_image_keys:
-            count = count + node_images[node_image_key]
-        logger.info("received info successful for %s, %d types, %d running instances" % (self.client_address[0], len(node_image_keys), count))
+        node_image_keys = node_scores.keys()
+        logger.info("received info successful for %s, %d types" % (self.client_address[0], len(node_image_keys)))
 
 def send_local_data(broadcast_port):
     try:
@@ -55,7 +52,7 @@ def send_local_data(broadcast_port):
     except:
         logger.error("Failed to load local node info")
         return
-    
+
     raw_data = opener.read()
     try:
         data = json.loads(raw_data)
@@ -67,7 +64,25 @@ def send_local_data(broadcast_port):
         logger.error("failed to parse data for local node")
         return
 
-    out_data = {'type':'dvmps_node_update_v1','images':data['images']}
+    images = {}
+    image_names = []
+    image_scores = []
+    total_images = 0
+
+    for image in data['images']:
+        if type(image).__name__ != 'dict' or not image.has_key('running_instances') or type(image['running_instances']).__name__ != 'int' or \
+           not image.has_key('base_image_name') or (type(image['base_image_name']).__name__ != 'str' and type(image['base_image_name']).__name__ != 'unicode'):
+            logger.error("malformed local data")
+            continue
+
+        image_names.append(image['base_image_name'])
+        images[image['base_image_name']] = image['running_instances']
+        total_images = total_images + image['running_instances']
+
+    for image_name in image_names:
+        image_scores.append({'base_image_name':image_name, 'score': 2 * images[image_name] - total_images})
+
+    out_data = {'type':'dvmps_node_update_v2','image_scores':image_scores}
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -77,20 +92,10 @@ def send_local_data(broadcast_port):
 
 def prune_stale_data():
     now = int(time.time())
-    node_keys = received_node_data.keys()
+    node_keys = received_node_scores.keys()
     for node_key in node_keys:
-        if now > received_node_data[node_key]['timestamp'] + 30:
-            del received_node_data[node_key]
-
-def get_image_node_score(image_name, node_name):
-    score = 0
-    node_image_names = received_node_data[node_name]['images']
-    for node_image_name in node_image_names:
-        if image_name == node_image_name:
-            score = score + received_node_data[node_name]['images'][node_image_name]
-        else:
-            score = score - received_node_data[node_name]['images'][node_image_name]
-    return score
+        if now > received_node_scores[node_key]['timestamp'] + 30:
+            del received_node_scores[node_key]
 
 def score_sort_helper(item):
     return -item['score']
@@ -101,9 +106,9 @@ def calculate_and_publish_placement_strategy():
     available_images = {}
     image_keys = []
 
-    node_keys = received_node_data.keys()
+    node_keys = received_node_scores.keys()
     for node_key in node_keys:
-        node_image_keys = received_node_data[node_key]['images']
+        node_image_keys = received_node_scores[node_key]['image_scores']
         for node_image_key in node_image_keys:
             if available_images.has_key(node_image_key):
                 if node_key not in available_images[node_image_key]:
@@ -117,7 +122,7 @@ def calculate_and_publish_placement_strategy():
     for image_key in image_keys:
         scores = []
         for node_key in available_images[image_key]:
-            scores.append({'node':node_key, 'score':get_image_node_score(image_key, node_key)})
+            scores.append({'node':node_key, 'score':received_node_scores[node_key]['image_scores'][image_key]})
         sorted_scores = sorted(scores, key=score_sort_helper)
         node_list = []
         for node_image_score in sorted_scores:
