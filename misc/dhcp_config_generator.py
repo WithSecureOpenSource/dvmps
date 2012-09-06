cfcbe3d7c60cab03e1b088d186e2ae377b97a2cb
Marko Räihä <marko.raiha@f-secure.com>
2012-09-06 08:59:51 +0300

    DHCPD now gives IP's only to the known hosts. Also relocated host definitions inside subnet definition as it should be.

--- misc/dhcp_config_generator.py
+++ misc/dhcp_config_generator.py
@@ -16,9 +16,9 @@ if __name__ == '__main__':
         sys.exit(-1)
 
     for ip in ipv4addr.ipv4addr_range(sys.argv[1], sys.argv[2]):
-        print "host dyn-%s {" % ip.replace('.', '-')
+        print "  host dyn-%s {" % ip.replace('.', '-')
         mac_addr = map(lambda n: "%02x" % ord(n), socket.inet_aton(ip)[-3:])
-        print "  hardware ethernet 00:aa:ee:%s;" % ":".join(mac_addr)
-        print "  fixed-address %s;" % ip
-        print "}"
+        print "    hardware ethernet 00:aa:ee:%s;" % ":".join(mac_addr)
+        print "    fixed-address %s;" % ip
+        print "  }"
         print ""
